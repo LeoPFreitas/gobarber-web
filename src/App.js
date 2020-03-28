7b05dcae302d94ded9a61e54bd850830e63da904
LeoPFreitas <leonardo.cadu2@hotmail.com>
2020-03-28 16:07:48 -0300

    config basic routes and reactotron

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
+import { Router } from 'react-router-dom';
+import history from './services/history';
+
+import './config/ReactotronConfig';
+
+import Routes from './routes';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Router history={history}>
+      <Routes />
+    </Router>
   );
 }
 
