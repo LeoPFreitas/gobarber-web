8b2c6f374a1e2f9dec49a9617aef38946d020d20
LeoPFreitas <leonardo.cadu2@hotmail.com>
2020-03-29 12:37:35 -0300

    add style to signin and signup

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function SignIn() {
-  return <h1>login</h1>;
+  return (
+    <>
+      <img src="https://via.placeholder.com/300x150" alt="Logo gobarber web" />
+
+      <form action="">
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </form>
+    </>
+  );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function SignUp() {
-  return <div></div>;
+  return (
+    <>
+      <img src="https://via.placeholder.com/300x150" alt="Logo gobarber web" />
+
+      <form action="">
+        <input type="text" placeholder="Nome completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
