1b79bd118cf76072f27e9d4f56435bc2a44df86e
LeoPFreitas <leonardo.cadu2@hotmail.com>
2020-03-30 21:44:22 -0300

    feat user authentication

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
+import { persistStore } from 'redux-persist';
 import crateSagaMiddleware from 'redux-saga';
+
 import createStore from './createStore';
+import persistReducer from './persistReducers';
 
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
@@ -13,8 +16,9 @@ const sagaMiddleware = crateSagaMiddleware({ sagaMonitor });
 
 const middlewares = [sagaMiddleware];
 
-const store = createStore(rootReducer, middlewares);
+const store = createStore(persistReducer(rootReducer), middlewares);
+const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga);
 
-export default store;
+export { store, persistor };
