c360078b3eec508c9b2450cfc48d774adea4cac7
LeoPFreitas <leonardo.cadu2@hotmail.com>
2020-04-03 20:36:00 -0300

    add dashboard style

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import api from '~/services/api';
 
-export default function Dashboard() {
-  api.get('appointments');
+import { Container, Time } from './styles';
 
-  return <h1>Dashboard</h1>;
+export default function Dashboard() {
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>20 Dezembro</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>09:00</strong>
+          <span>Leonardo Freitas</span>
+        </Time>
+        <Time available>
+          <strong>10:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Leonardo Freitas</span>
+        </Time>
+        <Time available>
+          <strong>12:00</strong>
+          <span>Em aberto</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
